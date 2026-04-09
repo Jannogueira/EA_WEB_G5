0ca2a4042b9524ae6c9f527eda5926f02d8548bb
Júlia Martínez <julia.caterina.martinez@estudiantat.upc.edu>
2026-04-09 11:37:14 +0200

    add models

---
+++ src/models/universidad.ts
@@ -0,0 +1,8 @@
+import { Usuario } from './usuario';
+
+export interface Universidad {
+    _id: string;
+    nombre: string;
+    ubicacion: string;
+    usuarios: Usuario[];
+}
---
+++ src/models/usuario.ts
@@ -0,0 +1,11 @@
+import { Universidad } from "./universidad";
+
+export interface Usuario {
+    _id: string;
+    nombre: string;
+    email: string;
+    password: string;
+    rol: 'admin' | 'user';
+    universidad?: Universidad;
+    activo: boolean;
+}
