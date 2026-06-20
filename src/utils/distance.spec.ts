6ef70c09e6b993968846d44e685fc64feb00c1c9
Laura <laura.manso@estudiantat.upc.edu>
2026-06-21 00:12:09 +0200

    v1.1.2 test

--- src/utils/distance.spec.ts
+++ src/utils/distance.spec.ts
@@ -5,7 +5,7 @@ describe('Distance Helper', () => {
   it('should calculate distance correctly between two close points', () => {
     // Barcelona center to UPC Campus Nord
     const lat1 = 41.38879; // Pl. Catalunya
-    const lon1 =     2.17006;
+    const lon1 = 2.17006;
     const lat2 = 41.3892; // UPC Campus Nord
     const lon2 = 2.113;
 
