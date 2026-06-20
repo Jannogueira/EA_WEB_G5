6a144407f23cf2a8e3bcd81aa9024eb66cf5e204
jannogueira <jannogueiraacosta@gmail.com>
2026-06-20 22:51:45 +0200

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,8 +38,9 @@ const Navbar: React.FC<NavbarProps> = ({ usuario: propUsuario }) => {
 
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <span className="brand-name" onClick={() => navigate("/home")}>Univy</span>
+      <div className="navbar-left" onClick={() => navigate("/home")}>
+        <img src="/logo.png" className="navbar-logo" alt="Univy Logo" />
+        <span className="brand-name">Univy</span>
       </div>
 
       <div className="navbar-right">
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -103,6 +103,9 @@ const Login = () => {
           />
         )}
         
+        <div className="login-logo-container">
+          <img src="/logo.png" className="login-logo" alt="Univy Logo" />
+        </div>
         <h2>{t('login.title')}</h2>
 
         <form onSubmit={handleLogin} className="login-form">
