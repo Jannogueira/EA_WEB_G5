acaf07e0db069e9fe3375c414a5845f5c2268771
0mar089 <omar.boury@estudiantat.upc.edu>
2026-06-22 01:31:13 +0200

    v1.4.2

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  apiUrl: 'http://localhost:1337',
+  apiUrl: 'https://ea5-api.upc.edu',
   googleClientId: '115506975798-184jljfmf2df7v2jl89kurg5rc302uh4.apps.googleusercontent.com',
 };
